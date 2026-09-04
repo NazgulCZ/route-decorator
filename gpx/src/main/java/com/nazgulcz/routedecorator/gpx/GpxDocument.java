a4dee9a8d71b64eae15214c09e5343aeda235430
NazgulCZ <63725903+NazgulCZ@users.noreply.github.com>
2026-09-04 07:50:07 +0200

    Add GpxDocument JAXB model to gpx module

---
+++ gpx/src/main/java/com/nazgulcz/routedecorator/gpx/GpxDocument.java
@@ -0,0 +1,30 @@
+package com.nazgulcz.routedecorator.gpx;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * JAXB model for the root GPX document.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@XmlRootElement(name = "gpx")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class GpxDocument {
+    @XmlElement(name = "wpt")
+    private List<GpxWaypoint> waypoints;
+
+    @XmlElement(name = "rte")
+    private List<GpxRoute> routes;
+
+    @XmlElement(name = "trk")
+    private List<GpxTrack> tracks;
+}
