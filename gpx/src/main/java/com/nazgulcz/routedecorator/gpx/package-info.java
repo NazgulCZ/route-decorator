60e755da08cf5d027543357a62837732100e316e
NazgulCZ <63725903+NazgulCZ@users.noreply.github.com>
2026-09-04 09:54:28 +0200

    Add package-info for gpx JAXB namespace

---
+++ gpx/src/main/java/com/nazgulcz/routedecorator/gpx/package-info.java
@@ -0,0 +1,11 @@
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlSchema(
+        namespace = "http://www.topografix.com/GPX/1/1",
+        elementFormDefault = XmlNsForm.QUALIFIED
+)
+package com.nazgulcz.routedecorator.gpx;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlNsForm;
+import jakarta.xml.bind.annotation.XmlSchema;
